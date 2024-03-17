fe80ac0a2c19de453077f193adc44a250a66c6f8
mail@vanheusden.com <Folkert van Heusden>
2024-03-17 22:29:39 +0100

    reduced loc

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import copy
 from data import *
 from nicegui import ui
 
-with ui.tabs().classes('w-full') as tabs_t:
-    tabs_t_one = ui.tab('per hour')
-    tabs_t_two = ui.tab('per day-of-week')
-    tabs_t_three = ui.tab('per month')
-
-with ui.tab_panels(tabs_t, value=tabs_t_one).classes('w-full'):
+def create_fig_card(data, title):
     fig_template = {
         'data': [
             {
@@ -27,61 +22,32 @@ with ui.tab_panels(tabs_t, value=tabs_t_one).classes('w-full'):
         },
     }
 
-    with ui.tab_panel(tabs_t_one):
-        with ui.card():
-            records_per_hour = get_record_count_per_hour()
-            fig = copy.deepcopy(fig_template)
-            fig['data'][0]['name'] = 'records per hour'
-            for item in records_per_hour:
-                fig['data'][0]['x'].append(item[0])
-                fig['data'][0]['y'].append(item[1])
-            ui.plotly(fig).classes('w-100 h-80')
+    fig = copy.deepcopy(fig_template)
+    fig['data'][0]['name'] = title
+    for item in data:
+        fig['data'][0]['x'].append(item[0])
+        fig['data'][0]['y'].append(item[1])
 
-        with ui.card():
-            unique_ip_count_per_hour = get_unique_ip_count_per_hour()
-            fig2 = copy.deepcopy(fig_template)
-            fig2['data'][0]['name'] = 'unique IP4/6 addr per hour'
-            for item in unique_ip_count_per_hour:
-                fig2['data'][0]['x'].append(item[0])
-                fig2['data'][0]['y'].append(item[1])
-            ui.plotly(fig2).classes('w-100 h-80')
+    with ui.card():
+        ui.label(title)
+        ui.plotly(fig).classes('w-100 h-80')
+
+with ui.tabs().classes('w-full') as tabs_t:
+    tabs_t_one = ui.tab('per hour')
+    tabs_t_two = ui.tab('per day-of-week')
+    tabs_t_three = ui.tab('per month')
+
+with ui.tab_panels(tabs_t, value=tabs_t_one).classes('w-full'):
+    with ui.tab_panel(tabs_t_one):
+        create_fig_card(get_record_count_per_hour(), 'records per hour')
+        create_fig_card(get_unique_ip_count_per_hour(), 'unique IP4/6 addresses per hour')
 
     with ui.tab_panel(tabs_t_two):
-        with ui.card():
-            records_per_hour = get_record_count_per_day_of_week()
-            fig = copy.deepcopy(fig_template)
-            fig['data'][0]['name'] = 'records per day-of-week'
-            for item in records_per_hour:
-                fig['data'][0]['x'].append(item[0])
-                fig['data'][0]['y'].append(item[1])
-            ui.plotly(fig).classes('w-100 h-80')
-
-        with ui.card():
-            unique_ip_count_per_day_of_week = get_unique_ip_count_per_day_of_week()
-            fig = copy.deepcopy(fig_template)
-            fig['data'][0]['name'] = 'unique IP4/6 addr per day-of-week'
-            for item in unique_ip_count_per_day_of_week:
-                fig['data'][0]['x'].append(item[0])
-                fig['data'][0]['y'].append(item[1])
-            ui.plotly(fig).classes('w-100 h-80')
+        create_fig_card(get_record_count_per_day_of_week(), 'records per day-of-week')
+        create_fig_card(get_unique_ip_count_per_day_of_week(), 'unique IP4/6 addresses per day-of-week')
 
     with ui.tab_panel(tabs_t_three):
-        with ui.card():
-            records_per_month = get_record_count_per_month()
-            fig = copy.deepcopy(fig_template)
-            fig['data'][0]['name'] = 'records per month'
-            for item in records_per_month:
-                fig['data'][0]['x'].append(item[0])
-                fig['data'][0]['y'].append(item[1])
-            ui.plotly(fig).classes('w-100 h-80')
-
-        with ui.card():
-            unique_ips_per_month = get_unique_ip_count_per_month()
-            fig = copy.deepcopy(fig_template)
-            fig['data'][0]['name'] = 'unique IP4/6 addr per month'
-            for item in unique_ips_per_month:
-                fig['data'][0]['x'].append(item[0])
-                fig['data'][0]['y'].append(item[1])
-            ui.plotly(fig).classes('w-100 h-80')
+        create_fig_card(get_record_count_per_month(), 'records per month')
+        create_fig_card(get_unique_ip_count_per_month(), 'unique IP4/6 addr per month')
 
 ui.run(show=False)
