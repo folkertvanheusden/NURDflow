2979d82e5f627cb78041b91ca238128852220702
mail@vanheusden.com <Folkert van Heusden>
2024-03-17 23:49:02 +0100

    dark mode

--- main.py
+++ main.py
@@ -75,29 +75,36 @@ def create_fig_card_per(data, title, items):
         ui.label(title)
         ui.plotly(fig).classes('w-100 h-80')
 
-with ui.tabs().classes('w-full') as tabs_t:
-    tabs_t_one = ui.tab('per hour')
-    tabs_t_two = ui.tab('per day-of-week')
-    tabs_t_three = ui.tab('per month')
+with ui.column():
+    dark = ui.dark_mode()
 
-with ui.tab_panels(tabs_t, value=tabs_t_one).classes('w-full'):
-    ports = (80, 443, 5900, 123)
-    with ui.tab_panel(tabs_t_one):
-        with ui.row():
-            create_fig_card(get_record_count_per_hour(), 'records per hour', False)
-            create_fig_card(get_unique_ip_count_per_hour(), 'unique IP4/6 addresses per hour', True)
-            create_fig_card_per(get_count_per_port_per_hour(ports), 'count per dst-port', ports)
+    with ui.row():
+        ui.label('Switch mode:')
+        ui.button('Dark', on_click=dark.enable)
+        ui.button('Light', on_click=dark.disable)
 
-    with ui.tab_panel(tabs_t_two):
-        with ui.row():
-            create_fig_card(get_record_count_per_day_of_week(), 'records per day-of-week', False)
-            create_fig_card(get_unique_ip_count_per_day_of_week(), 'unique IP4/6 addresses per day-of-week', True)
-            create_fig_card_per(get_count_per_port_per_day_of_week(ports), 'count per dst-port', ports)
+    with ui.tabs().classes('w-full') as tabs_t:
+        tabs_t_one = ui.tab('per hour')
+        tabs_t_two = ui.tab('per day-of-week')
+        tabs_t_three = ui.tab('per month')
 
-    with ui.tab_panel(tabs_t_three):
-        with ui.row():
-            create_fig_card(get_record_count_per_month(), 'records per month', False)
-            create_fig_card(get_unique_ip_count_per_month(), 'unique IP4/6 addr per month', True)
-            create_fig_card_per(get_count_per_port_per_month(ports), 'count per dst-port', ports)
+    with ui.tab_panels(tabs_t, value=tabs_t_one).classes('w-full'):
+        ports = (80, 443, 5900, 123)
+        with ui.tab_panel(tabs_t_one):
+            with ui.row():
+                create_fig_card(get_record_count_per_hour(), 'records per hour', False)
+                create_fig_card(get_unique_ip_count_per_hour(), 'unique IP4/6 addresses per hour', True)
+                create_fig_card_per(get_count_per_port_per_hour(ports), 'count per dst-port', ports)
 
+        with ui.tab_panel(tabs_t_two):
+            with ui.row():
+                create_fig_card(get_record_count_per_day_of_week(), 'records per day-of-week', False)
+                create_fig_card(get_unique_ip_count_per_day_of_week(), 'unique IP4/6 addresses per day-of-week', True)
+                create_fig_card_per(get_count_per_port_per_day_of_week(ports), 'count per dst-port', ports)
+
+        with ui.tab_panel(tabs_t_three):
+            with ui.row():
+                create_fig_card(get_record_count_per_month(), 'records per month', False)
+                create_fig_card(get_unique_ip_count_per_month(), 'unique IP4/6 addr per month', True)
+                create_fig_card_per(get_count_per_port_per_month(ports), 'count per dst-port', ports)
 ui.run(show=False)
